8ea835c5e1eca656bb1ed60c981d41753d60a63d
Sandro Janusch <sjanusch@seibert-media.net>
2016-08-16 09:24:56 +0200

    Webhook deativiert

--- src/main/java/de/sjanusch/Main.java
+++ src/main/java/de/sjanusch/Main.java
@@ -2,16 +2,15 @@ package de.sjanusch;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-
 import de.sjanusch.guice.GuiceModule;
 
 public class Main {
 
   public static void main(final String[] args) {
     final Injector injector = Guice.createInjector(new GuiceModule());
-    final Webhook webhook = injector.getInstance(Webhook.class);
+    //final Webhook webhook = injector.getInstance(Webhook.class);
     final Xmpp xmpp = injector.getInstance(Xmpp.class);
-    webhook.run();
+    //webhook.run();
     xmpp.run();
   }
 }
